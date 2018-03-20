8f68d079ab081a4c83a76cebdbb2770d23cc486d
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-03-20 09:56:34 +0200

    Fixed stuff

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import { logErrors, errorHandler } from './error'
+import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// setup the logger
+app.use(morgan('tiny'))
+
+// Priority serve static files.
+app.use(express.static(path.join(__dirname, '../front-end/build')))
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Handling errors
+app.use(logErrors)
+app.use(errorHandler)
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+// For everything else return React so react can do extra routing
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../front-end/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log("-----------starting-----------")
+  console.log(`Listening on port ${PORT}`)
+});
