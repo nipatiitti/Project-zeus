bef63d65d64731c9e050576a5ad7f42569bf0184
Dingodhole <lukas.luotola@windowslive.com>
2019-04-27 19:14:27 +0300

    Renamed front-end

--- front-end/src/reducers/main.js
+++ front-end/src/reducers/main.js
@@ -1,17 +1,80 @@
-import { combineReducers } from 'redux'
+/**
+ * Login reducer
+ *
+ *
+ */
 
-import error from './error'
-import loading from './loading'
-import info from './info'
-import user from './user'
-import channels from './channels'
+import moment from 'moment'
 
-const main = combineReducers({
-  error,
-  loading,
-  info,
-  user,
-  channels
-})
+import {
+    CODE_SUCCESS,
+    CHANNELS,
+    CHANNELS_SUCCESS,
+    CHANNELS_FAIL,
+    USER,
+    USER_SUCCESS,
+    USER_FAIL,
+    TOKEN,
+    TOKEN_SUCCESS,
+    TOKEN_FAIL
+} from 'actions/types'
 
-export default main
+const initialState = {
+    code: null,
+    token: null,
+    expires_in: null,
+    user: null,
+    channels: null,
+    loading: false
+}
+
+export const main = (state = initialState, action) => {
+    switch (action.type) {
+        case CODE_SUCCESS:
+            return {
+                ...state,
+                code: action.code
+            }
+
+        case CHANNELS:
+        case CHANNELS_FAIL:
+        case USER:
+        case USER_FAIL:
+        case TOKEN:
+        case TOKEN_FAIL:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case CHANNELS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                channels: action.payload.data
+            }
+
+        case USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                user: action.payload.data
+            }
+
+        case TOKEN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                token: action.payload.data.access_token,
+                expires_in: moment()
+                    .add(action.payload.data.expires_in, 's')
+                    .format()
+            }
+
+        case 'LOGOUT':
+            return initialState
+
+        default:
+            return state
+    }
+}
